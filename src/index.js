55ffb696af6372fbd639d5a2f248d8f3f79ecbaf
John Whiles <johnwhiles@outlook.com>
2017-08-31 21:20:42 +0100

    actually fix ports.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const Express = require('express');
 
 const server = require('./server.js');
 
-const port = process.env.PORT || 8000,
+const port = process.env.PORT || 8000;
 
 //runs the server
 server.listen(port, () => {
-  console.log('listening on port 3000');
+  console.log(`listening on port ${port}`);
 });
