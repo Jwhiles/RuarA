ab6cdc8f19719e46f77ca05055d28f39895e43ac
John Whiles <johnwhiles@outlook.com>
2017-08-31 21:17:44 +0100

    make the port work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const Express = require('express');
 
 const server = require('./server.js');
 
+const port = process.env.PORT || 8000,
+
 //runs the server
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on port 3000');
 });
