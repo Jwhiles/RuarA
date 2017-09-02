5372018f4d3d71278c8abc510d97e68e9625701c
John Whiles <johnwhiles@outlook.com>
2017-09-02 13:36:08 +0100

    start to build lookbook page

---
+++ public/main.js
@@ -0,0 +1,29 @@
+
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    lookbook: [
+      {
+        description: 'Graphic cape in wool crepe, red heart boots in suede',
+        image: 'src',
+        number: "001",
+      },
+      {
+        description: 'Graphic cape in wool crepe, red heart boots in suede',
+        image: 'src',
+        number: "002",
+      },
+      {
+        description: 'Graphic cape in wool crepe, red heart boots in suede',
+        image: 'src',
+        number: "003",
+      },
+      {
+        description: 'Graphic cape in wool crepe, red heart boots in suede',
+        image: 'src',
+        number: "004",
+      },
+    ]
+  }
+})
