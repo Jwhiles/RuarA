78aa29f03d577d593d5465886336bab008d378a5
John Whiles <johnwhiles@outlook.com>
2017-09-07 20:07:31 +0100

    add descriptions

--- public/main.js
+++ public/main.js
@@ -5,89 +5,136 @@ var app4 = new Vue({
   data: {
     lookbook: [
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
+        description:
+        ['SLIP DRESS WITH PANELING DETAIL IN SILK CREPE DE CHINE',],
         number: "001",
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "002",
+        description: [
+'SLEEVELESS SHIRT DRESS IN SILK/COTTON POPLIN',
+'BAG WORN AROUND WAIST IN NAVY LEATHER AND SILVER HARDWARE',
+        ],
+        number: "002"
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "003",
+        description: [
+'SHIRT IN SILK',
+'MENS STRIPED JACKET IN WOOL/COTTON MIX',
+'3/4 LENGTH TROUSERS IN MERINO WOOL',
+        ],
+        number: '003',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "004",
+        description: [
+'VEST IN BAMBOO JERSEY',
+'SHIRT IN SILK CREPE DE CHINE',
+'TROUSERS IN MERINO WOOL',
+        ],
+        number: '004',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "005",
+        description: [
+'TURBAN IN SILK/COTTON MIX POPLIN',
+'BACKLESS SWIMSUIT IN NEOPRENE',
+        ],
+        number: '005',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "006",
+        description: [
+'CREW NECK SWEATER IN PURE NEW WOOL',
+'SHIRT IN SILK CREPE DE CHINE',
+'3/4 LENGTH TROUSERS IN MERINO WOOL',
+'BAG IN NAVY LEATHER WITH SILVER HARDWARE',
+        ],
+        number: '006',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "007",
+        description: [
+'SLASH NECK, KIMONO SLEEVE TOP IN BAMBOO JERSEY',
+'3/4 LENGTH TROUSERS IN MERINO WOOL',
+        ],
+        number: '007',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "008",
+        description: [
+'STRIPE SHIRT IN COTTON POPLIN',
+'BUTTON UP SKIRT WITH 4 SPLITS IN COTTON',
+        ],
+        number: '008',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "009",
+        description: [
+'BACKLESS DRESS WITH RUFFLE DETAIL IN SILK/COTTON POPLIN',
+        ],
+        number: '009',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "010",
+        description: [
+'SHIRT IN SILK CREPE DE CHINE',
+'JACKET WITH PATCH POCKETS IN LINEN',
+'TROUSERS IN MERINO WOOL',
+        ],
+        number: '010',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "011",
+        description: [
+'SLEEVELESS SWEATER WITH “XIV” PATCH IN PURE NEW WOOL',
+'DRESS WITH PLEAT SLEEVE DETAIL IN SILK CREPE DE CHINE',
+        ],
+        number: '011',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "012",
+        description: [
+'CROPPED JACKET WITH PATCH POCKETS IN LINEN',
+'DRESS IN SILK GEORGETTE',
+        ],
+        number: '012',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "013",
+        description: [
+'VEST IN BAMBOO JERSEY',
+'SHIRT IN SILK CREPE DE CHINE',
+'STRIPED BLAZER WITH PATCH POCKETS IN COTTON/WOOL MIX',
+'TROUSERS IN MERINO WOOL',
+        ],
+        number: '013',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "014",
+        description: [
+'SILVER SPRAY PAINTED FEATHER HEADDRESS',
+'COCKTAIL DRESS WITH CAPE SLEEVE DETAIL IN SILK GEORGETTE',
+        ],
+        number: '014',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "015",
+        description: [
+'EVENING DRESS WITH PANELING AND EMBROIDERED RUFFLE DETAIL IN SILK CREPE DE CHINE',
+        ],
+        number: '015',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "016",
+        description: [
+'DOUBLE-BREASTED JACKET WITH SHAWL COLLAR IN SILK MATKA',
+'TROUSERS IN SILK MATKA',
+        ],
+        number: '016',
       },
       {
-        description: 'Graphic cape in wool crepe, red heart boots in suede',
-        image: 'src',
-        number: "017",
+        description: [
+'EVENING DRESS WITH DART DETAIL AT SIDE IN SILK GEORGETTE',
+        ],
+        number: '017',
       },
+      {
+        description: [
+'GLENGARRY HAT IN MERINO WOOL',
+'JACKET WITH SHAWL COLLAR AND PATCH POCKET IN SILK MATKA',
+'SHIRT IN SILK CREPE DE CHINE',
+'TROUSERS IN SILK MATKA',
+        ],
+        number: '018',
+      }
     ]
   }
 })
+
