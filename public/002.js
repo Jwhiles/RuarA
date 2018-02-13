83fb67ce5d59befa40b88be84c25d994495220e4
John Whiles <johnwhiles@outlook.com>
2018-02-13 19:20:27 +0000

    update for second collection
    remove yarn

---
+++ public/002.js
@@ -0,0 +1,96 @@
+var app = new Vue({
+  el: '#app-2',
+  data: {
+    lookbook: [
+      {
+        description: ['DOUBLE-BREASTED SKIRT SUIT IN HARRIS TWEED'],
+        number: '001',
+      },
+      {
+        description: [
+          'HERRINGBONE OVERCOAT, TIE, CHECK DOUBLE-BREASTED BLAZER AND PLEAT TROUSER ALL IN HARRIS TWEED',
+        ],
+        number: '002',
+      },
+      {
+        description: [
+          'HERRINGBONE COAT AND SKIRT SUIT ALL IN HARRIS TWEED',
+        ],
+        number: '003',
+      },
+      {
+        description: [
+          'HERRINGBONE COAT AND SKIRT SUIT ALL IN HARRIS TWEED',
+        ],
+        number: '004',
+      },
+      {
+        description: [
+          'TIE, CHECK BLAZER AND SINGLE PLEAT TROUSER ALL IN HARRIS TWEED',
+        ],
+        number: '005',
+      },
+      {
+        description: [
+          'SINGLE-BREASTED COAT IN OVERCHECK HOUNDSTOOTH HARRIS TWEED',
+        ],
+        number: '006',
+      },
+      {
+        description: [
+          'DOUBLE-BREASTED COAT IN OVERCHECK HERRINGBONE HARRIS TWEED',
+        ],
+        number: '007',
+      },
+      {
+        description: [
+          'V-NECK DRESS IN HERRINGBONE HARRIS TWEED',
+        ],
+        number: '008',
+      },
+      {
+        description: [
+          'TARTAN SCARF IN HARRIS TWEED AND SILK. SINGLE-BREASTED COAT IN HERRINGBONE HARRIS TWEED',
+        ],
+        number: '009',
+      },
+      {
+        description: [
+          'TIE, CHECK BLAZER AND SINGLE PLEAT TROUSER ALL IN HARRIS TWEED',
+        ],
+        number: '010',
+      },
+      {
+        description: [
+          'OFF THE SHOULDER DRESS IN HARRIS TWEED',
+        ],
+        number: '011',
+      },
+      {
+        description: [
+          'SCARF IN HARRIS TWEED AND SILK. OPERA COAT IN CHECK HARRIS TWEED',
+        ],
+        number: '012',
+      },
+      {
+        description: [
+          'PINK TUXEDO JACKET IN SILK AND SINGLE PLEAT TROUSER IN HARRIS TWEED',
+          'CHARCOAL GREY TUXEDO IN HARRIS TWEED AND SILK'
+        ],
+        number: '013',
+      },
+      {
+        description: [
+          'SHAWL IN MONGOLIAN FUR. EMBROIDERED, OFF THE SHOULDER EVENING GOWN IN HARRIS TWEED',
+        ],
+        number: '014',
+      },
+      {
+        description: [
+          'OFF THE SHOULDER EVENING GOWN IN METALLIC SILK',
+        ],
+        number: '015',
+      },
+    ],
+  },
+});
