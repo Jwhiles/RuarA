be2174ef910066f03ef2d72b781c6e8de5a4f0f8
John Whiles <jwhiles@Johns-MacBook-Air-2.local>
2018-12-23 15:19:57 +0000

    Update with third collection

---
+++ public/003.js
@@ -0,0 +1,90 @@
+var app = new Vue({
+  el: '#app-2',
+  data: {
+    lookbook: [
+      {
+        description: ['HAND PAINTED SILK SATIN DRESS'],
+        number: '001',
+      },
+      {
+        description: [
+          'HAND PAINTED SILK SATIN SHIRT. LINEN SHORTS',
+        ],
+        number: '002',
+      },
+      {
+        description: [
+          'SILK SATIN SHIRT AND LINEN SHORTS',
+        ],
+        number: '003',
+      },
+      {
+        description: [
+          'SILK SATIN DRESS',
+        ],
+        number: '004',
+      },
+      {
+        description: [
+          'HAND PAINTED SILK SATIN SHIRT AND SILK SATIN WRAP SKIRT',
+        ],
+        number: '005',
+      },
+      {
+        description: [
+          'SILK SATIN DRESS',
+        ],
+        number: '006',
+      },
+      {
+        description: [
+          'HAND PINATED SILK SATIN SLEEVLESS SHIRT',
+        ],
+        number: '007',
+      },
+      {
+        description: [
+          'HAND PAINTED SILK SATIN DRESS',
+        ],
+        number: '008',
+      },
+      {
+        description: [
+          'SILK SATIN DRESS',
+        ],
+        number: '009',
+      },
+      {
+        description: [
+          'SILK SATIN SHIRT AND LINEN SHORTS',
+        ],
+        number: '010',
+      },
+      {
+        description: [
+          'SILK SATIN DRESS WITH FEATHER TRIM',
+        ],
+        number: '011',
+      },
+      {
+        description: [
+          'COTTON TUXEDO SHIRT AND LINEN TROUSERS',
+        ],
+        number: '012',
+      },
+      {
+        description: [
+          'LAYERED TULLE AND SILK SATIN DRESS',
+        ],
+        number: '013',
+      },
+      {
+        description: [
+          'HAND PAINTED SILK SATIN DRESS',
+        ],
+        number: '014',
+      },
+    ],
+  },
+});
+
